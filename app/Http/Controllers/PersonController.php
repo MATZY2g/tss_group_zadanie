f6bdc00b7c11185cea1e4ccd127d6e34f2b27305
Matzy2G <matzy3cs@gmail.com>
2024-06-20 21:43:51 +0200

    created model controller migration and endpoints for person

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $search = $request->query('search');
+        $persons = Person::query();
+
+        if ($search) {
+            $persons = $persons->where('name', 'like', '%$search')
+                               ->orWhere('age', 'like', '%$search')
+                               ->orWhere('gender', 'like', '%$search');
+        }
+
+        return response()->json($persons->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $person = Person::create($request->all());
+        return response()->json($person, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Person $person)
+    {
+        return response()->json($person);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Person $person)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Person $person)
+    {
+        $person->update($request->all());
+        return response()->json($person);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Person $person)
+    {
+        $person->delete();
+        return response()->json(null, 204);
+    }
+}
